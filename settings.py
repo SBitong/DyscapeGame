e0d37573190eac6eb58b62811647b2ca0a5d8b76
stian <stianbitong69@gmail.com>
2024-09-11 09:26:30 +0800

    changes RGB to color name

--- settings.py
+++ settings.py
@@ -9,6 +9,16 @@ GRAY = (128, 128, 128)
 FERN_GREEN = (79, 121, 66)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+DEEP_VIOLET = (54, 0, 102)
+DARK_BURGUNDY = (128, 0, 32)
+FOREST_GREEN = (34, 139, 34)
+DARK_TURQUOISE = (0, 102, 102)
+INDIGO = (75, 0, 130)
+DARK_MAGENTA = (139, 0, 139)
+SAPPHIRE_BLUE = (8, 37, 103)
+DARK_COPPER = (115, 63, 18)
+DARK_GOLD = (184, 134, 11)
+CRIMSON_RED = (153, 0, 0)
 
 # Master Volume (0.0 to 1.0)
 MASTER_VOLUME = 0.5
@@ -23,37 +33,37 @@ TTS_ENABLED = True
 # Set colors for words to be more distinguishable
 word_colors = {
     "The": GRAY,
-    "big": (173, 216, 230),
-    "brown": (160, 82, 45),
-    "fox": (34, 139, 34),
-    "jumps": (138, 43, 226),
-    "over": (255, 69, 0),
-    "the": (0, 100, 0),
-    "lazy": (255, 140, 0),
-    "dog.": (75, 0, 130),
-    "My": (255, 192, 203),
-    "house": (173, 216, 230),
-    "is": (160, 82, 45),
-    "and": (138, 43, 226),
-    "painted": (255, 69, 0),
-    "blue.": (0, 100, 0),
-    "I": (255, 140, 0),
-    "love": (75, 0, 130),
-    "my": (255, 192, 203),
-    "mom": (173, 216, 230),
-    "very.": (160, 82, 45),
-    "much": (34, 139, 34),
-    "cat": (255, 69, 0),
-    "likes": (0, 100, 0),
-    "to": (255, 140, 0),
-    "climb.": (75, 0, 130),
-    "like": (173, 216, 230),
-    "drink": (34, 139, 34),
+    "big": DEEP_VIOLET,
+    "brown": DARK_COPPER,
+    "fox": FOREST_GREEN,
+    "jumps": DARK_MAGENTA,
+    "over": CRIMSON_RED,
+    "the": INDIGO,
+    "lazy": DARK_BURGUNDY,
+    "dog.": BLACK,
+    "My": SAPPHIRE_BLUE,
+    "house": DEEP_VIOLET,
+    "is": DARK_TURQUOISE,
+    "and": DARK_GOLD,
+    "painted": DARK_MAGENTA,
+    "blue.": SAPPHIRE_BLUE,
+    "I": INDIGO,
+    "love": DEEP_VIOLET,
+    "my": CRIMSON_RED,
+    "mom": DARK_COPPER,
+    "very.": GRAY,
+    "much": FOREST_GREEN,
+    "cat": DARK_BURGUNDY,
+    "likes": CRIMSON_RED,
+    "to": DARK_MAGENTA,
+    "climb.": DARK_GOLD,
+    "like": DARK_BURGUNDY,
+    "drink": GRAY,
     "chocolate": (138, 43, 226),
     "milk.": (255, 69, 0),
     "Mark": (0, 100, 0),
     "goes": (255, 140, 0),
-    "school": (255, 192, 203),
+    "school": SAPPHIRE_BLUE,
     "every": (173, 216, 230),
     "morning.": (160, 82, 45),
     "fell": (255, 69, 0),
